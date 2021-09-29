a78d2f0478228e9262ad2fce1fc3eab5adf032f9
hareesh43 <hareeshgummani@gmail.com>
2021-09-29 13:10:02 +0530

    added crypto news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,91 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Select, Card, Row, Col, Typography, Avatar } from "antd";
+import moment from "moment";
 
-export default function News() {
-    return <div>News</div>;
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+const { Title, Text } = Typography;
+const { Option } = Select;
+const demoImage =
+  "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
+
+export default function News({ simplified }) {
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: newsCategory,
+    count: simplified ? 6 : 12,
+  });
+  const { data } = useGetCryptosQuery(100);
+
+  if (!cryptoNews?.value) return "Loading ...";
+
+  return (
+    <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            style={{ width: 200 }}
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            <Option value="Cryptocurrency">Cryptocurrency</Option>
+            {data?.data?.coins?.map((coin) => (
+              <Option value={coin.name} key={coin.id}>
+                {" "}
+                {coin.name}
+              </Option>
+            ))}
+          </Select>
+        </Col>
+      )}
+      {cryptoNews?.value?.map((news, i) => (
+        <Col xs={24} sm={12} lg={10} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noopener noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+                <img
+                  src={news?.image?.thumbnail?.contentUrl || demoImage}
+                  alt="news"
+                />
+              </div>
+              <p>
+                {news.description.length > 100
+                  ? `${news.description.substring(0, 100)}...`
+                  : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={
+                      news?.provider[0]?.image?.thumbnail?.contentUrl ||
+                      demoImage
+                    }
+                    alt=""
+                  />
+                  <Text className="provider-name">
+                    {news?.provider[0]?.name}
+                  </Text>
+                </div>
+                <Text className="provider-name">
+                  {moment(news?.datePublished).startOf("ss").fromNow()}
+                </Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "antd/dist/antd.css";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import {store} from "./app/store";
+
+ReactDOM.render(
+  <Provider store = {store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
